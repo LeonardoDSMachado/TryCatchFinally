954ffa1fbf67718cb3ef1f89304f1f0234debaed
Leonardo Machado <ldsm.leomachado@gmail.com>
2025-04-28 20:14:12 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿
+try
+{
+    Console.WriteLine("Digite seu nome\n");
+    string nome = Console.ReadLine();
+
+    Console.WriteLine("Digite sua idade\n");
+    int idade = Convert.ToInt32(Console.ReadLine());
+
+    if (idade < 0)
+    {
+        throw new ArgumentException("A idade não pode ser negativa");
+    }
+    else if (idade == 0)
+    {
+        throw new NotImplementedException("A idade ainda não foi definida");
+    }
+    else if (string.IsNullOrEmpty(nome))
+    {
+        throw new NullReferenceException("O nome não pode ser nulo nem vazio");
+    }
+    else 
+    {
+        Console.WriteLine("\nNome: " + nome);
+        Console.WriteLine("Idade: " + idade);
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine("\nErro: " + ex.Message);
+}
+finally
+{
+    Console.WriteLine("\nPrograma finalizado.");
+}
+
+Console.ReadKey();
